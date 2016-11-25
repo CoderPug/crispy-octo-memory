17e3118e01a1fe190a6c87c9c61cffe9670b2f0b
Jose Torres <jose.torres@santexgroup.com>
2016-11-25 18:46:40 -0300

    Updated Dictionary extension

--- TheMovieDB/TheMovieDBCore/TheMovieDBCore/Resources/Extensions/Dictionary.swift
+++ TheMovieDB/TheMovieDBCore/TheMovieDBCore/Resources/Extensions/Dictionary.swift
@@ -8,6 +8,18 @@
 
 extension Dictionary {
     
+    mutating func merge(with dictionary: Dictionary) {
+        
+        dictionary.forEach { updateValue($1, forKey: $0) }
+    }
+    
+    func merged(with dictionary: Dictionary) -> Dictionary {
+        
+        var dictionary = self
+        dictionary.merge(with: dictionary)
+        return dictionary
+    }
+    
     func httpParameters() -> String {
         
         let result = self.map { (key, value) -> String in
