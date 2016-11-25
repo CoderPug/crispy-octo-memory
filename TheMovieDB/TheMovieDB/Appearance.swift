3377c1ccfd363c9f4085a105adec0129efdf1c5f
Jose Torres <jose.torres@santexgroup.com>
2016-11-25 13:29:52 -0300

    Added Appearance struct

---
+++ TheMovieDB/TheMovieDB/Appearance.swift
@@ -0,0 +1,27 @@
+//
+//  Appearance.swift
+//  TheMovieDB
+//
+//  Created by Santex on 11/25/16.
+//  Copyright © 2016 coderpug. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Appearance {
+    
+    struct Colors {
+        
+        static let principal = UIColor.blue
+        static let secondary = UIColor.red
+    }
+    
+    struct Fonts {
+        
+        static let h1 = UIFont.boldSystemFont(ofSize: 15.0)
+        static let h2 = UIFont.boldSystemFont(ofSize: 13.0)
+        static let text = UIFont.systemFont(ofSize: 14.0, weight: 200)
+    }
+    
+}
