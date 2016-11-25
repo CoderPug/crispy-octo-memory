8aacfb108d7662261accd92b5caeef022cf82ad9
Jose Torres <jose.torres@santexgroup.com>
2016-11-25 11:53:09 -0300

    Added CMMovies with base implementation for requesting DiscoverMovies

---
+++ TheMovieDB/TheMovieDBCore/TheMovieDBCore/Managers/Connection/CMMovies.swift
@@ -0,0 +1,52 @@
+//
+//  CMMovies.swift
+//  TheMovieDBCore
+//
+//  Created by Santex on 11/25/16.
+//  Copyright © 2016 coderpug. All rights reserved.
+//
+
+import Foundation
+
+struct CMMoviesConstants {
+    
+    static let URIDiscoverMovies = "discover/movie"
+}
+
+struct CMMoviesJSONKeys {
+    
+    static let results = "results"
+}
+
+extension ConnectionManager {
+    
+    public func requestDiscoverMovies(_ handler: @escaping (Result<[Movie]>) -> Swift.Void ) {
+        
+        request(URI: CMMoviesConstants.URIDiscoverMovies) { data, response, error in
+            
+            guard let data = data, error == nil else {
+
+                return
+            }
+            
+            var movies: [Movie] = []
+            
+            guard let dictionary = data.toJSON(),
+                let results = dictionary[CMMoviesJSONKeys.results] as? [AnyObject] else {
+                    
+                    return
+            }
+            
+            for item in results {
+                
+                if let data = item as? [String: AnyObject] {
+                    
+                    dump(data)
+                }
+            }
+            
+            handler(.Success([]))
+        }
+    }
+    
+}
