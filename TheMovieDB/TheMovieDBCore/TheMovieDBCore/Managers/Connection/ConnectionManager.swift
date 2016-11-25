11dd56386f192907963987937b075e7720855356
Jose Torres <jose.torres@santexgroup.com>
2016-11-25 11:35:27 -0300

    Added ConnectionManager request implementation.

--- TheMovieDB/TheMovieDBCore/TheMovieDBCore/Managers/Connection/ConnectionManager.swift
+++ TheMovieDB/TheMovieDBCore/TheMovieDBCore/Managers/Connection/ConnectionManager.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+public enum Result<T> {
+    
+    case Failure(Error)
+    case Success(T)
+}
+
+public struct ConnectionManager {
+    
+    public var configuration: APIData?
+    
+    public init() {
+        
+    }
+    
+    func request(URI: String, handler: @escaping (Data?, URLResponse?, Error?) -> Swift.Void) {
+        
+        guard let configuration = configuration else {
+            
+            return
+        }
+        
+        let parameters = ["api_key": configuration.APIToken, "language": configuration.language].httpParameters()
+        
+        let url = URL(string:"\(configuration.serverURL)\(URI)?\(parameters)")!
+        
+        let task = URLSession.shared.dataTask(with: url, completionHandler: handler)
+        
+        task.resume()
+    }
+    
+}
