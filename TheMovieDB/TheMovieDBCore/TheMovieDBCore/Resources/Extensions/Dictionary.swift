d4c0c57f82255cf40b5a6ea3cf7be739493f000e
Jose Torres <jose.torres@santexgroup.com>
2016-11-25 11:34:53 -0300

    Added Dictionary extension with function for getting string of concatenated values for sending over http request as parameters.

--- TheMovieDB/TheMovieDBCore/TheMovieDBCore/Resources/Extensions/Dictionary.swift
+++ TheMovieDB/TheMovieDBCore/TheMovieDBCore/Resources/Extensions/Dictionary.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2016 coderpug. All rights reserved.
 //
 
-import Foundation
+extension Dictionary {
+    
+    func httpParameters() -> String {
+        
+        let result = self.map { (key, value) -> String in
+            
+            guard let convertedKey = (key as? String)?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+                let convertedValue = (value as? String)?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+                    
+                    return ""
+            }
+            
+            return "\(convertedKey)=\(convertedValue)"
+        }
+        
+        return result.joined(separator: "&")
+    }
+    
+}
