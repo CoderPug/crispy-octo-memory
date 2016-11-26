16873f209501e8f5bd0977c5b42bf30002abf749
Jose Torres <jose.torres@santexgroup.com>
2016-11-26 12:55:34 -0300

    Added implementation on CMMovies for requesting movie genres.

--- TheMovieDB/TheMovieDBCore/TheMovieDBCore/Managers/Connection/CMMovies.swift
+++ TheMovieDB/TheMovieDBCore/TheMovieDBCore/Managers/Connection/CMMovies.swift
@@ -11,7 +11,11 @@ import Foundation
 struct CMMoviesConstants {
     
     static let URIDiscoverMovies = "discover/movie"
+    static let URIGenre = "genre/movie/list"
+    
     static let URLParamPage = "page"
+    
+    static let JSONGenreList = "genres"
 }
 
 struct CMMoviesJSONKeys {
@@ -44,6 +48,46 @@ extension ConnectionManager {
             }
             
             handler(.Success(moviePage))
+        }
+    }
+    
+    public func requestGenre(handler: @escaping (Result<[MovieGenre]>) -> Swift.Void ) {
+        
+        request(URI: CMMoviesConstants.URIGenre) { data, response, error in
+            
+                    
+            guard let data = data, error == nil else {
+                
+                handler(.Failure(error ?? TheMovieDBError.unkwnon))
+                return
+            }
+                    
+            guard let dictionary = data.toJSON() else {
+                
+                handler(.Failure(TheMovieDBError.JSONParseFailed(reason: "")))
+                return
+            }
+            
+            guard let content = dictionary[CMMoviesConstants.JSONGenreList] as? Array<AnyObject> else {
+                        
+                handler(.Failure(TheMovieDBError.missingKey))
+                return
+            }
+            
+            var result: [MovieGenre] = []
+            
+            for item in content {
+                
+                if let data = item as? [String: AnyObject] {
+                    
+                    if let genre = MovieGenre(data: data) {
+                        
+                        result.append(genre)
+                    }
+                }
+            }
+            
+            handler(.Success(result))
         }
     }
     
