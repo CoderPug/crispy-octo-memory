2a3d37d99fb42f3fef17f1cb11837bf570bde37a
Jose Torres <jose.torres@santexgroup.com>
2016-11-27 21:49:10 -0500

    Added base watchOS target.

---
+++ TheMovieDB/TheMovieDBWatchOS Extension/MovieListInterfaceController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieListInterfaceController.swif
+//  TheMovieDBWatchOS Extension
+//
+//  Created by Santex on 11/27/16.
+//  Copyright © 2016 coderpug. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import TheMovieDBCore
+
+class MovieListInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+    }
+    
+    override func willActivate() {
+        
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        
+        super.didDeactivate()
+    }
+
+}
